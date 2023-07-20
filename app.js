bf207b81e2dffe7165f9b8b295b5e6a1fdce14a5
Adam Nguyen <Adanatonw@gmail.com>
2023-07-19 20:33:11 -0400

    setting up middleware for better functionality

--- app.js
+++ app.js
@@ -4,16 +4,13 @@ const tasks = require("./routes/tasks");
 
 const connectDB = require("./db/connect");
 require("dotenv").config();
+const notFound = require("./middleware/not-found");
 // middle ware
+app.use(express.static("./public"));
 app.use(express.json());
 
-//routes
-app.get("/hello", (req, res) => {
-  res.send("Task manager app");
-});
-
 app.use("/api/v1/tasks", tasks);
-
+app.use(notFound);
 const port = 3000;
 
 const start = async () => {
