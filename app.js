2c8b4757c53da3df817fffc1ec65061eebec60c0
Adam Nguyen <Adanatonw@gmail.com>
2023-07-19 21:07:46 -0400

    improved error handling functionality

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
 require("dotenv").config();
 const notFound = require("./middleware/not-found");
+const errorHandler = require("./middleware/error-handler");
 // middle ware
 app.use(express.static("./public"));
 app.use(express.json());
 
 app.use("/api/v1/tasks", tasks);
 app.use(notFound);
+app.use(errorHandler);
 const port = 3000;
 
 const start = async () => {
